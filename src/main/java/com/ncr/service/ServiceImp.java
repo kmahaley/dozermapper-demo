e8b76c1002fa3ef46581e6e1dda94ee6ce91d874
Mahaley, Kartik <Kartik.Mahaley@ncr.com>
2018-01-18 22:56:58 -0500

    add different port number

--- src/main/java/com/ncr/service/ServiceImp.java
+++ src/main/java/com/ncr/service/ServiceImp.java
@@ -18,13 +18,16 @@ import com.ncr.model.PersonViewData;
  * @author kartik
  */
 @RestController
-@RequestMapping("/services")
+@RequestMapping("/mapping-services")
 public class ServiceImp {
 
-    DozerBeanMapper mapper= new DozerBeanMapper();
+    DozerBeanMapper mapper = new DozerBeanMapper();
 
-    @RequestMapping(value = "/{salary}",method = RequestMethod.POST)
-    public PersonViewData readBookmarks(@RequestBody PersonData input, @RequestParam("age") int age, @PathVariable("salary") String salary ) {
+    @RequestMapping(value = "/{dozer}", method = RequestMethod.POST)
+    public PersonViewData postDozer(
+            @RequestBody PersonData input,
+            @RequestParam("age") int age,
+            @PathVariable("salary") String salary) {
         PersonIdData id = new PersonIdData();
         id.setId(UUID
                 .randomUUID()
@@ -34,13 +37,24 @@ public class ServiceImp {
         personViewData.setIdData(id);
         personViewData.setSalary(Integer.valueOf(salary));
         personViewData.setAge(age);
+
         return personViewData;
 
     }
 
+    @RequestMapping(value = "/{dozer}", method = RequestMethod.GET)
+    public String getDozer(@PathVariable("var") Long var) {
+        return "getDozer-" + var;
+    }
 
-    @RequestMapping(value = "/{age}", method = RequestMethod.GET)
-    public String post(@PathVariable("age") Long age) {
-        return "hello-" + age;
+
+    @RequestMapping(value = "/{orika}", method = RequestMethod.POST)
+    public String postOrika() {
+        return "postOrika";
+    }
+
+    @RequestMapping(value = "/{orika}", method = RequestMethod.GET)
+    public String getOrika(@PathVariable("var") Long var) {
+        return "getOrika-" + var;
     }
 }
